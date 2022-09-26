978b7caaecdbc009186a8629f7ca8d5c4149e38b
EkorrTi <balagazy.daur@gmail.com>
2022-09-27 01:02:57 +0600

    Basic Firebase support

--- app/src/main/java/com/example/diploma/MainActivity.kt
+++ app/src/main/java/com/example/diploma/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.diploma
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.diploma.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.messaging.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+            Toast.makeText(applicationContext, "Yes Notifications", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "No Notifications", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,5 +48,22 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            val tag = "FIREBASE"
+        if (!task.isSuccessful) {
+            Log.w(tag, "Fetching FCM registration token failed", task.exception)
+            return@OnCompleteListener
+        }
+
+        // Get new FCM registration token
+        val token = task.result
+
+        // Log and toast
+        Log.d(tag, "Token - $token")
+        Toast.makeText(baseContext, "Token - $token", Toast.LENGTH_SHORT).show()
+    })
     }
+
+
 }
